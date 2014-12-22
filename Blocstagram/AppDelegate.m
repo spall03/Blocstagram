cbf35b29a315179e9dbae86b158f22552aeac10c
Steve Palley <spall03@gmail.com>
2014-12-22 11:12:14 -0800

    NSIndexPath bug

--- Blocstagram/AppDelegate.m
+++ Blocstagram/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "BLCImagesTableViewController.h"
 
 @interface AppDelegate ()
 
@@ -15,9 +16,14 @@
 @implementation AppDelegate
 
 
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    self.window.rootViewController = [[UINavigationController alloc] initWithRootViewController:[[BLCImagesTableViewController alloc] init]];
+    self.window.backgroundColor = [UIColor whiteColor];
+    [self.window makeKeyAndVisible];
     return YES;
+    
 }
 
 - (void)applicationWillResignActive:(UIApplication *)application {
---
+++ Blocstagram/BLCImagesTableViewController.h
@@ -0,0 +1,14 @@
+//
+//  BLCImagesTableViewController.h
+//  Blocstagram
+//
+//  Created by Stephen Palley on 12/19/14.
+//  Copyright (c) 2014 Steve Palley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BLCImagesTableViewController : UITableViewController
+@property (nonatomic, strong) NSMutableArray *images;
+
+@end
